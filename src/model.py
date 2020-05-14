fda000f7bd05a3b2ecf0dbed28bf0aa9f58a56bc
Kilam <Malikanhar@users.noreply.github.com>
2020-05-14 15:58:41 +0700

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.metrics import BinaryAccuracy, Precision, Recall, AUC
+from tensorflow.keras.models import Model, load_model
+
+def get_model(model, input_shape):
+    '''
+    Get the base model of the `model` name
+
+    Parameters
+    ----------
+    model : String representing the base model used (vgg16, inception_v3, or resnet50)
+    input_shape  : Input shape for the model, should be (height, width, 3)
+    
+    Return
+    ------
+        Model with imagenet weights provided by tensorflow
+    '''
+
+    return {
+        'vgg16' : tf.keras.applications.vgg16.VGG16(include_top=False, weights='imagenet', input_shape=input_shape),
+        'inception_v3' : tf.keras.applications.inception_v3.InceptionV3(include_top=False, weights='imagenet', input_shape=input_shape),
+        'resnet50' : tf.keras.applications.resnet.ResNet50(include_top=False, weights='imagenet', input_shape=input_shape)
+    }[model]
+
+def build_model(model, input_shape, num_classes):
+    '''
+    Get the base model of the `model` name
+
+    Parameters
+    ----------
+    model : String representing the base model used (vgg16, inception_v3, or resnet50)
+    input_shape  : Input shape for the model, should be (height, width, 3)
+    num_classes  : The number of classes for the output dense
+    
+    Return
+    ------
+        Model with the classification head
+    '''
+
+    base_model = get_model(model, input_shape)
+    base_model.trainable = False
+
+    x = GlobalAveragePooling2D()(base_model.output)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    # For the multilabel data, we use sigmoid activation instead of softmax
+    predictions = Dense(num_classes, activation='sigmoid')(x)
+
+    return Model(inputs=base_model.input, outputs=predictions)
+
+def define_metrics():
+    '''Define the training metrics.'''
+    return [
+      BinaryAccuracy(name='accuracy'),
+      Precision(name='precision'),
+      Recall(name='recall'),
+      AUC(name='auc'),
+    ]
+
+def save_model(model, model_dir, model_filename):
+    '''Save the model in model_dir with model_filename as the filename.'''
+    model.save(os.path.join(model_dir, model_filename))
+    print('Model saved at {}'.format(model_dir))
+
+def load_model(model_path):
+    '''Load pretrained model.'''
+    return load_model(model_path)
