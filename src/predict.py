a3ef6f6a3dafe257943c3f46836e33f1fa201766
Kilam <Malikanhar@users.noreply.github.com>
2020-05-16 17:12:57 +0700

    Added face detection using MTCNN

--- src/predict.py
+++ src/predict.py
@@ -13,52 +13,121 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.models import load_model
 from utils import transform_image
+from mtcnn.mtcnn import MTCNN
 
-def convertAttribute(attributes, threshold = 0.5):
+def detect_face(img):
+    '''
+    Face detection using MTCNN
+
+    Parameters
+    ----------
+    img : 3D numpy array with shape (height, width, channel)
+
+    Return
+    ------
+        faces  : List of face images that have been cropped according to their bbox
+        bboxes : List of bbox with values of x, y, w, h
+    '''
+
+    faces = []
+    bboxes = []
+    detector = MTCNN()
+    detected_faces = detector.detect_faces(img)
+
+    for detected_face in detected_faces:
+        bbox = detected_face['box']
+        
+        x_offset = int(bbox[2] * 0.2)
+        y_offset = int(bbox[3] * 0.125)
+
+        x1 = max(0, bbox[0] - x_offset)
+        y1 = max(0, (bbox[1] - int(2 * y_offset)))
+        x2 = min(img.shape[1], bbox[0] + bbox[2] + x_offset)
+        y2 = min(img.shape[0], bbox[1] + bbox[3] + y_offset)
+        
+        faces.append(img[y1 : y2, x1 : x2])
+        bboxes.append(bbox)
+
+    return faces, bboxes
+
+def convert_attribute(bboxes, predictions):
     '''
     Converting an array of probability to its corresponding label
 
     Parameters
     ----------
-    attributes : 1D numpy array with length 4 which have values in the range of 0 to 1
+    bboxes      : List of bbox with values of x, y, w, h
+    predictions : 2D numpy array with shape (n, 4) which have values in the range of 0 to 1
+                  n is the number of faces detected by MTCNN
 
     Return
     ------
-        label : Dictionary with key as the label and value as the confidence level in percentage
+        outputs : List of dictionary consisting of bbox and its probability of each classes
+                example => [{
+                    'bbox' : [x, y, w, h]
+                    'classes' : {
+                        'Eyeglassess' : eyeglasses_confident,
+                        'Mustache' : mustache_confident,
+                        'Beard' : beard_confident,
+                        'Hat' : hat_confident
+                    }
+                }]
     '''
 
     attribute_names = ['Eyeglasses', 'Mustache', 'Beard', 'Hat']
-    labels = {}
-    for i, attr in enumerate(attribute_names):
-        # If the probability of a class is more than the threshold then it will be set with its appropriate label
-        if attributes[i] > threshold:
-            labels[attribute_names[i]] = '{:.1%}'.format(attributes[i])
-    return labels
+    outputs = []
+    for i, bbox in enumerate(bboxes):
+        output = {}
+        classes = {}
+        prediction = predictions[i]
+        for j in range(len(attribute_names)):
+            classes[attribute_names[j]] = prediction[j]
+        outputs.append({
+            'bbox' : bbox,
+            'classes' : classes
+        })
+    return outputs
 
-def draw_outputs(img, labels):
+def draw_outputs(img, annotations, threshold = 0.5, height = 480):
     '''
     Put the label to the image
 
     Parameters
     ----------
     img : numpy array with size of (224, 224)
-    label : Dictionary with key as the label and value as the confidence level in percentage
+    annotations : List of dictionary consisting of bbox and its probability of each classes
 
     Return
     ------
-    numpy array of an image with its label
+        numpy array of resized image with labels for each detected faces
     '''
-    x1y1 = (10, 224)
-    y_txt1 = 10
-    y_txt2 = 5
-    for key in labels.keys():
-        text = '{} {}'.format(key, labels[key])
-        color = (0, 255, 255)
-        img = cv2.rectangle(img, (x1y1[0] - 5, x1y1[1] - (y_txt1 + 11)), (x1y1[0] + (len(key) * 8) + 40, x1y1[1] - y_txt2), color, -1)
-        img = cv2.putText(img, text, (x1y1[0], x1y1[1] - y_txt1), cv2.FONT_HERSHEY_DUPLEX, 0.4, (0, 0, 0), 1)
-        y_txt1 += 20
-        y_txt2 += 20
-    return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    color = (0, 255, 255)
+
+    ratio = img.shape[1] / img.shape[0]
+    width = int(height * ratio)
+    res_img = cv2.resize(img, (width, height))
+    
+    for annotation in annotations:
+        bbox = annotation['bbox']
+        label = annotation['classes']
+
+        x1y1 = (int(bbox[0] * height / img.shape[0]), int(bbox[1] * width / img.shape[1]))
+        x2y2 = (int((bbox[0] + bbox[2]) * height / img.shape[0]), int((bbox[1] + bbox[3]) * width / img.shape[1]))
+        y_txt1 = 10
+        y_txt2 = 5
+
+        res_img = cv2.rectangle(res_img, x1y1, x2y2, color, 2)
+        
+        for key in label.keys():
+            # If the probability of a class is more than the threshold then it will be set with its appropriate label
+            if label[key] > threshold:
+                text = '{} {:.1%}'.format(key, label[key])
+                res_img = cv2.rectangle(res_img, (x1y1[0], x1y1[1] - y_txt1 - 11), (x1y1[0] + (len(key) * 8) + 60, x1y1[1] - y_txt2), color, -1)
+                res_img = cv2.putText(res_img, text, (x1y1[0] + 5, x1y1[1] - y_txt1), cv2.FONT_HERSHEY_DUPLEX, 0.4, (0, 0, 0), 1)
+                y_txt1 += 20
+                y_txt2 += 20
+    return cv2.cvtColor(res_img, cv2.COLOR_RGB2BGR)
 
 def main():
     parser = argparse.ArgumentParser(description='Parser for Predicting Face Attribute')
@@ -80,26 +149,34 @@ def main():
     filename = args.image
     size = (args.img_size, args.img_size)
 
+    results = None
+
+    # Load model
+    model = load_model(model_path)
+
     # Load image
     img = np.array(load_img(filename))
 
-    # Preprocess the image and expand its dimension
-    trans_img = transform_image(img, size, preprocessing)
-    trans_img = np.expand_dims(trans_img, 0)
+    # Detect faces using MTCNN face detector
+    faces, bboxes = detect_face(img)
 
-    # Load model and predict the image
-    model = load_model(model_path)
-    pred = model.predict(trans_img)[0]
-    
-    # Convert the output of predict to the label
-    label = convertAttribute(pred)
-    print(label)
+    if faces is not None:
+        # Preprocess the image and expand its dimension
+        trans_imgs = [transform_image(face, size, preprocessing) for face in faces]
 
-    # Save the output image
-    if args.save_image:
-        img = draw_outputs(cv2.resize(img, size), label)
-        cv2.imwrite('out_' + filename, img)
-        print('Output image saved at out_{}'.format(filename))
+        # Predict face attributes
+        preds = model.predict(np.array(trans_imgs))
+
+        # Convert the output of predict and combine with its corresponding bbox
+        results = convert_attribute(bboxes, preds)
+
+        # Save the output image
+        if args.save_image:
+            img = draw_outputs(img, results)
+            cv2.imwrite('out_{}'.format(filename), img)
+            print('Output image saved at out_{}'.format(filename))
+
+    print(results)
 
 if __name__ == "__main__":
     main()
